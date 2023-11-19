b7da044f040c36d4004846322068744d81981edc
Marlou Pepito <marloupepito07@gmail.com>
2023-11-19 10:44:03 +0800

    dajdlkajldla

---
+++ public/build/assets/recipes-services-811a5ca1.js
@@ -0,0 +1,1 @@
+import{k as s}from"./app-ef09a1a0.js";async function r(){return(await s.get("/api/get_all_recipes")).data.status}async function n(e){return(await s.post("/api/create_recipes",e)).data}async function c(e){return(await s.delete("/api/delete_recipes/"+e)).data}export{n as c,c as d,r as g};
